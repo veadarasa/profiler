f550f418b16af6b79c31fb924fe3f06d55e1e7a6
veadarasa <vea@darasa.io>
2024-07-03 16:52:49 +0400

    Build navigate Live chat page
    Build navigate Live chat page

--- src/NavigateLiveChat.tsx
+++ src/NavigateLiveChat.tsx
@@ -1,27 +1,11 @@
+import React, { useEffect, useState } from "react";
+
 import axios from "axios";
-import { format } from 'date-fns';
-import {
-  ArrowRight2,
-  CloseCircle
-} from "iconsax-react";
-import ReactPlayer from "react-player";
-import React, { useEffect, useRef, useState } from "react";
-import { Button, ProgressBar } from "react-bootstrap";
-import ReactLoading from "react-loading";
-import { ToastContainer, toast } from "react-toastify";
+import { Button } from "react-bootstrap";
 import "react-toastify/dist/ReactToastify.css";
 import "./navigate-livechat.scss";
 
 const URL_ENDPOINT = process.env.REACT_APP_API_URL;
-
-const BRAND_CODE = "g88";
-
-const KEY_MAPPING = {
-  DOC_SIZE: "inv_doc_img_size",
-  DOC_TYPE: "inv_doc_img_type",
-  VIDEO_SIZE: "inv_doc_video_size",
-  VIDEO_TYPE: "inv_doc_video_type",
-}
 
 const BRAND_LIST: {
   [key: number]: string;
@@ -39,98 +23,184 @@ const isMobile = navigator.userAgent.match(/mobile/i);
 const Brand_dummy_data = {
     brandId: "4103",
     logo: "4103",
-    newDomain: "HITCLUP.TOP",
+    domain: "HITCLUP.TOP",
     brandName: "HIT.CLUP",
-    domain: "HIT.CLUP",
+    domain1: "HIT.CLUP",
     withdrawUrl: "/withdraw",
     depositUrl: "/deposit",
-    forgotPasswordUrl: "/forgotPassword",
-    anotherIssueUrl: "/other",
-    phoneUrl: "/phone",
+    forgetUrl: "/forgotPassword",
+    otherUrl: "/other",
+    phone: "/phone",
     fbUrl: "/fb",
-    teleUrl: "/tele"
+    telegram: "/tele",
+    brand: "hitclup"
 }
 function NavigateLiveChat() {
-  const imageUploadRef: any = useRef();
-  const videoUploadRef: any = useRef();
-//   const [uploadedImages, setUploadedImages] = useState<UploadedImage[]>([]);
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const currentUrl = window.location.pathname;
+
+    // Split the URL path by '/'
+  const brand = currentUrl.split('/')[2];
+  const xtoken = urlParams.get('xtoken');
 
 
-  const handleClick = (url: string) => {  
-
+  const [ brandData, setBrandData ] = useState({
+    "brandId": "4103",
+    "brandName": "",
+    "domain": "",
+    "background": null,
+    "title": "",
+    "depositUrl": "",
+    "withdrawUrl": "",
+    "forgetUrl": "",
+    "otherUrl": "",
+    "phone": "1234567890",
+    "facebook": "",
+    "telegram": "",
+    "recommendations": null
+  });
+  const handleClick = (url: string, paramTracking: string) => {
+    logActionTracking(paramTracking);
+    window.location.href = url;
 
   }
-  const renderButton = (name: string, url: string) => {
-    return <Button onClick={() => handleClick(url)}><span>{name}</span></Button>
+  const renderButton = (name: string, url: string, paramTracking: string) => {
+    return <Button onClick={() => handleClick(url, paramTracking)}><span>{name}</span></Button>
   }
+  function useOnceCall(cb: any, condition = true) {
+    const isCalledRef = React.useRef(false);
+
+    useEffect(() => {
+      if (condition && !isCalledRef.current) {
+        isCalledRef.current = true;
+        cb();
+      }
+    }, [cb, condition]);
+  }
+
+  useOnceCall(() => {
+    getConfig();
+  });
+
+
+  const getConfig = async () => {
+    try {
+
+      const getData = await axios.get(
+        `${URL_ENDPOINT}support/api/v1/config/${brand}?xtoken=${xtoken}`
+      );
+      if (getData.status === 200) {
+        setBrandData(getData?.data);
+        const brandId: number = getData.data?.brandId;
+        document.title = BRAND_LIST?.[brandId];
+
+        const iconLink = document.createElement('link');
+        iconLink.rel = 'icon';
+        iconLink.href = `/images/${brandId}-fav.png`;
+
+        const appleTouchIconLink = document.createElement('link');
+        appleTouchIconLink.rel = 'apple-touch-icon';
+        appleTouchIconLink.href = `/images/${brandId}-fav.png`;
+
+        document.head.appendChild(iconLink);
+        document.head.appendChild(appleTouchIconLink);
+        
+      }
+    } catch (error) {
+      setBrandData({
+        "brandId": "",
+        "brandName": "",
+        "domain": "",
+        "background": null,
+        "title": "",
+        "depositUrl": "",
+        "withdrawUrl": "",
+        "forgetUrl": "",
+        "otherUrl": "",
+        "phone": "1234567890",
+        "facebook": "",
+        "telegram": "",
+        "recommendations": null
+      })
+    }
+  };
+
+  const logActionTracking = async (paramTracking: string) => {
+    try {
+      await axios.post(
+        `${URL_ENDPOINT}support/api/v1/log/${brand}?xtoken=${xtoken}&action=${paramTracking}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       {!isMobile && (
         <div className="sub-actions">
           <div className="sub-button">
-            <img id={Brand_dummy_data.brandId} src={`/images/phone.svg`} className={`main-logo logo-name`} />
+            <img id={brandData.brandId} src={`/images/phone.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.phone, 'PHONE')} />
           </div>
           <div className="sub-button">
-            <img id={Brand_dummy_data.brandId} src={`/images/fb.svg`} className={`main-logo logo-name`} />
+            <img id={brandData.brandId} src={`/images/fb.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.facebook, 'FB')} />
           </div>
           <div className="sub-button">
-            <img id={Brand_dummy_data.brandId} src={`/images/tele.svg`} className={`main-logo logo-name`} />
+            <img id={brandData.brandId} src={`/images/tele.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.telegram, 'TELE')} />
           </div>
         </div>)
       }
       <article className="navigate-live-chat bg-1">
         <div className="bg-2"/>
-        <div className="group-content">
-          <div className="center-content">
-            <div className="logo row">
-                <img id={Brand_dummy_data.brandId} src={`/images/${Brand_dummy_data.logo}.png`} className={`main-logo logo-name`} />
-            </div>
-            <div className="msg row">
-              <span>Quý Khách vui lòng chọn nội dung cần hỗ trợ bên dưới:</span>
-            </div>
-            <div className="action-group ">
-              <div className="button-group">
-                {/* <div className="row"> */}
-                  {renderButton("NẠP", Brand_dummy_data.withdrawUrl)}
-                {/* </div>
-                <div className="row"> */}
-                  {renderButton("RÚT", Brand_dummy_data.depositUrl)}
-                {/* </div>
-                <div className="row"> */}
-                    {renderButton("QUÊN MẬT KHẨU", Brand_dummy_data.forgotPasswordUrl)}
-                {/* </div>
-                <div className="row"> */}
-                    {renderButton("VẤN ĐỀ KHÁC", Brand_dummy_data.anotherIssueUrl)}
-                {/* </div> */}
-              </div>
-            </div>
-            {isMobile && (
-              <div className="sub-action-mb">
-                {/* <div className="sub-button"> */}
-                  <img id={Brand_dummy_data.brandId} src={`/images/phone.svg`} className={`main-logo logo-name`} />
-                {/* </div> */}
-                {/* <div className="sub-button"> */}
-                  <img id={Brand_dummy_data.brandId} src={`/images/fb.svg`} className={`main-logo logo-name`} />
-                {/* </div>
-                <div className="sub-button"> */}
-                  <img id={Brand_dummy_data.brandId} src={`/images/tele.svg`} className={`main-logo logo-name`} />
-                {/* </div> */}
-              </div>
-            )}
-            <div className="footer">
-                <div className="content">
-                    <div>
-                      <span className="highlight">{Brand_dummy_data.newDomain}</span> là tên miền mới nhất của <span className="highlight">{Brand_dummy_data.brandName}</span></div>
+        
+        {brandData.brandId ? 
+          <div className="group-content">
+              <div className="center-content">
+                <div className="logo row">
+                  {brandData.brandId &&  <img id={brandData.brandId} src={`/images/${brandData.brandId}.png`} className={`main-logo logo-name`} />}
                   
                 </div>
-                <div className="content">
-                <div>Quý khách có thể truy cập <span className="highlight">{Brand_dummy_data.domain}</span> để kiểm tra chính hãng.</div>
-                  
+                <div className="msg row">
+                  <span>Quý Khách vui lòng chọn nội dung cần hỗ trợ bên dưới:</span>
                 </div>
+                <div className="action-group ">
+                  <div className="button-group">
+                      {renderButton("NẠP", brandData.withdrawUrl, "deposit")}
+                      {renderButton("RÚT", brandData.depositUrl, "withdraw")}
+                      {renderButton("QUÊN MẬT KHẨU", brandData.forgetUrl, "forg`et")}
+                      {renderButton("VẤN ĐỀ KHÁC", brandData.otherUrl, "other")}`
+                  </div>
+                </div>
+                {isMobile && (
+                  <div className="sub-action-mb">
+                      <img id={brandData.brandId} src={`/images/phone.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.phone, 'PHONE')} />
+                      <img id={brandData.brandId} src={`/images/fb.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.facebook, 'FB')}/>
+                      <img id={brandData.brandId} src={`/images/tele.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.telegram, 'TELE')}/>
+                  </div>
+                )}
+                {brandData.domain &&
+                  <div className="footer">
+                    <div className="content">
+                        <div>
+                          <span className="highlight">{brandData.domain}</span> là tên miền mới nhất của <span className="highlight">{brandData.brandName}</span></div>
+                      </div>
+                    <div className="content">
+                      <div>Quý khách có thể truy cập <span className="highlight"><a href={`http://${brandData.brandName}`}>{brandData.brandName}</a></span> để kiểm tra chính hãng.</div>
+                    </div>
+                  </div>
+                }
                 
-            </div>
+              </div>
           </div>
-        </div>
+          :
+          <div className="not-found-container">
+            <div>
+              <h1>404 - Page Not Found</h1>
+              <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+            </div>
+            
+          </div>
+        }
         
       </article>
     </div>
