91e2125df5a428b64eb8f453ef404539cb1d2c94
veadarasa <vea@darasa.io>
2024-07-26 13:09:54 +0400

    fix/[BC114103N-4247][Support Live Chat][FE] Hiển thị "404 - Page Not Found" khi click icon button điện thoại
    fix/[BC114103N-4247][Support Live Chat][FE] Hiển thị "404 - Page Not Found" khi click icon button điện thoại

--- src/NavigateLiveChat.tsx
+++ src/NavigateLiveChat.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import axios from "axios";
-import { Button } from "react-bootstrap";
 import "react-toastify/dist/ReactToastify.css";
 import "./navigate-livechat.scss";
+import { Button, Tooltip, Overlay } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const URL_ENDPOINT = process.env.REACT_APP_API_URL;
 
@@ -16,58 +17,84 @@ const BRAND_LIST: {
   2120: "X8",
   8116: "TOP88",
   9070: "FA88",
-  4118: "VIC"}
+  4118: "VIC",
+};
 
 const isMobile = navigator.userAgent.match(/mobile/i);
 
 const Brand_dummy_data = {
-    brandId: "4103",
-    logo: "4103",
-    domain: "HITCLUP.TOP",
-    brandName: "HIT.CLUP",
-    domain1: "HIT.CLUP",
-    withdrawUrl: "/withdraw",
-    depositUrl: "/deposit",
-    forgetUrl: "/forgotPassword",
-    otherUrl: "/other",
-    phone: "/phone",
-    fbUrl: "/fb",
-    telegram: "/tele",
-    brand: "hitclup"
-}
+  brandId: "4103",
+  logo: "4103",
+  domain: "HITCLUP.TOP",
+  brandName: "HIT.CLUP",
+  domain1: "HIT.CLUP",
+  withdrawUrl: "/withdraw",
+  depositUrl: "/deposit",
+  forgetUrl: "/forgotPassword",
+  otherUrl: "/other",
+  phone: "/phone",
+  fbUrl: "/fb",
+  telegram: "/tele",
+  brand: "hitclup",
+};
 function NavigateLiveChat() {
-
   const urlParams = new URLSearchParams(window.location.search);
   const currentUrl = window.location.pathname;
 
-    // Split the URL path by '/'
-  const brand = currentUrl.split('/')[2];
-  const xtoken = urlParams.get('xtoken');
+  // Split the URL path by '/'
+  const brand = currentUrl.split("/")[2];
+  const xtoken = urlParams.get("xtoken");
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [target, setTarget] = useState(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLImageElement>(null);
 
-
-  const [ brandData, setBrandData ] = useState({
-    "brandId": "4103",
-    "brandName": "",
-    "domain": "",
-    "background": null,
-    "title": "",
-    "depositUrl": "",
-    "withdrawUrl": "",
-    "forgetUrl": "",
-    "otherUrl": "",
-    "phone": "1234567890",
-    "facebook": "",
-    "telegram": "",
-    "recommendations": null
+  const [brandData, setBrandData] = useState({
+    brandId: "4103",
+    brandName: "",
+    domain: "",
+    background: null,
+    title: "",
+    depositUrl: "",
+    withdrawUrl: "",
+    forgetUrl: "",
+    otherUrl: "",
+    phone: "1234567890",
+    facebook: "",
+    telegram: "",
+    recommendations: null,
   });
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (
+        tooltipRef.current &&
+        !tooltipRef.current.contains(event.target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        setShowTooltip(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   const handleClick = (url: string, paramTracking: string) => {
     logActionTracking(paramTracking);
     window.location.href = url;
-
-  }
+  };
   const renderButton = (name: string, url: string, paramTracking: string) => {
-    return <Button onClick={() => handleClick(url, paramTracking)}><span>{name}</span></Button>
-  }
+    return (
+      <Button onClick={() => handleClick(url, paramTracking)}>
+        <span>{name}</span>
+      </Button>
+    );
+  };
   function useOnceCall(cb: any, condition = true) {
     const isCalledRef = React.useRef(false);
 
@@ -83,10 +110,8 @@ function NavigateLiveChat() {
     getConfig();
   });
 
-
   const getConfig = async () => {
     try {
-
       const getData = await axios.get(
         `${URL_ENDPOINT}support/api/v1/config/${brand}?xtoken=${xtoken}`
       );
@@ -95,35 +120,40 @@ function NavigateLiveChat() {
         const brandId: number = getData.data?.brandId;
         document.title = BRAND_LIST?.[brandId];
 
-        const iconLink = document.createElement('link');
-        iconLink.rel = 'icon';
+        const iconLink = document.createElement("link");
+        iconLink.rel = "icon";
         iconLink.href = `/images/${brandId}-fav.png`;
 
-        const appleTouchIconLink = document.createElement('link');
-        appleTouchIconLink.rel = 'apple-touch-icon';
+        const appleTouchIconLink = document.createElement("link");
+        appleTouchIconLink.rel = "apple-touch-icon";
         appleTouchIconLink.href = `/images/${brandId}-fav.png`;
 
         document.head.appendChild(iconLink);
         document.head.appendChild(appleTouchIconLink);
-        
       }
     } catch (error) {
       setBrandData({
-        "brandId": "",
-        "brandName": "",
-        "domain": "",
-        "background": null,
-        "title": "",
-        "depositUrl": "",
-        "withdrawUrl": "",
-        "forgetUrl": "",
-        "otherUrl": "",
-        "phone": "1234567890",
-        "facebook": "",
-        "telegram": "",
-        "recommendations": null
-      })
+        brandId: "",
+        brandName: "",
+        domain: "",
+        background: null,
+        title: "",
+        depositUrl: "",
+        withdrawUrl: "",
+        forgetUrl: "",
+        otherUrl: "",
+        phone: "1234567890",
+        facebook: "",
+        telegram: "",
+        recommendations: null,
+      });
     }
+  };
+
+  const handleClickPhone = (event: any) => {
+    !showTooltip && logActionTracking("PHONE");
+    setTarget(event.target);
+    setShowTooltip(!showTooltip);
   };
 
   const logActionTracking = async (paramTracking: string) => {
@@ -140,71 +170,152 @@ function NavigateLiveChat() {
       {!isMobile && (
         <div className="sub-actions">
           <div className="sub-button">
-            <img id={brandData.brandId} src={`/images/phone.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.phone, 'PHONE')} />
+            <img
+              id={brandData.brandId}
+              src={`/images/phone.svg`}
+              className={`main-logo logo-name`}
+              onClick={handleClickPhone}
+              ref={buttonRef}
+            />
+            <Overlay
+              target={target}
+              show={showTooltip}
+              placement="top"
+              ref={tooltipRef}
+            >
+              {(props) => (
+                <Tooltip
+                  id="tooltip-right"
+                  {...props}
+                  className="custom-tooltip"
+                >
+                  {brandData?.phone}
+                </Tooltip>
+              )}
+            </Overlay>
           </div>
           <div className="sub-button">
-            <img id={brandData.brandId} src={`/images/fb.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.facebook, 'FB')} />
+            <img
+              id={brandData.brandId}
+              src={`/images/fb.svg`}
+              className={`main-logo logo-name`}
+              onClick={() => handleClick(brandData?.facebook, "FB")}
+            />
           </div>
           <div className="sub-button">
-            <img id={brandData.brandId} src={`/images/tele.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.telegram, 'TELE')} />
+            <img
+              id={brandData.brandId}
+              src={`/images/tele.svg`}
+              className={`main-logo logo-name`}
+              onClick={() => handleClick(brandData?.telegram, "TELE")}
+            />
           </div>
-        </div>)
-      }
+        </div>
+      )}
       <article className="navigate-live-chat bg-1">
-        <div className="bg-2"/>
-        
-        {brandData.brandId ? 
+        <div className="bg-2" />
+
+        {brandData.brandId ? (
           <div className="group-content">
-              <div className="center-content">
-                <div className="logo row">
-                  {brandData.brandId &&  <img id={brandData.brandId} src={`/images/${brandData.brandId}.png`} className={`main-logo logo-name`} />}
-                  
-                </div>
-                <div className="msg row">
-                  <span>Quý Khách vui lòng chọn nội dung cần hỗ trợ bên dưới:</span>
-                </div>
-                <div className="action-group ">
-                  <div className="button-group">
-                      {renderButton("NẠP", brandData.depositUrl, "deposit")}
-                      {renderButton("RÚT", brandData.withdrawUrl, "withdraw")}
-                      {renderButton("QUÊN MẬT KHẨU", brandData.forgetUrl, "forget")}
-                      {renderButton("VẤN ĐỀ KHÁC", brandData.otherUrl, "other")}`
-                  </div>
-                </div>
-                {isMobile && (
-                  <div className="sub-action-mb">
-                      <img id={brandData.brandId} src={`/images/phone.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.phone, 'PHONE')} />
-                      <img id={brandData.brandId} src={`/images/fb.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.facebook, 'FB')}/>
-                      <img id={brandData.brandId} src={`/images/tele.svg`} className={`main-logo logo-name`} onClick={() => handleClick(brandData?.telegram, 'TELE')}/>
-                  </div>
+            <div className="center-content">
+              <div className="logo row">
+                {brandData.brandId && (
+                  <img
+                    id={brandData.brandId}
+                    src={`/images/${brandData.brandId}.png`}
+                    className={`main-logo logo-name`}
+                  />
                 )}
-                {brandData.domain &&
-                  <div className="footer">
-                    <div className="content">
-                        <div>
-                          <span className="highlight">{brandData.domain}</span> là tên miền mới nhất của <span className="highlight">{brandData.brandName}</span></div>
-                      </div>
-                    <div className="content">
-                      <div>Quý khách có thể truy cập <span className="highlight"><a href={`http://${brandData.brandName}`}>{brandData.brandName}</a></span> để kiểm tra chính hãng.</div>
+              </div>
+              <div className="msg row">
+                <span>
+                  Quý Khách vui lòng chọn nội dung cần hỗ trợ bên dưới:
+                </span>
+              </div>
+              <div className="action-group ">
+                <div className="button-group">
+                  {renderButton("NẠP", brandData.depositUrl, "deposit")}
+                  {renderButton("RÚT", brandData.withdrawUrl, "withdraw")}
+                  {renderButton("QUÊN MẬT KHẨU", brandData.forgetUrl, "forget")}
+                  {renderButton("VẤN ĐỀ KHÁC", brandData.otherUrl, "other")}`
+                </div>
+              </div>
+              {isMobile && (
+                <div className="sub-action-mb">
+                  <img
+                    id={brandData.brandId}
+                    src={`/images/phone.svg`}
+                    className={`main-logo logo-name`}
+                    onClick={handleClickPhone}
+                    ref={buttonRef}
+                  />
+                  <Overlay
+                    target={target}
+                    show={showTooltip}
+                    placement="top"
+                    ref={tooltipRef}
+                  >
+                    {(props) => (
+                      <Tooltip
+                        id="tooltip-right"
+                        {...props}
+                        className="custom-tooltip"
+                      >
+                        {brandData?.phone}
+                      </Tooltip>
+                    )}
+                  </Overlay>
+                  <img
+                    id={brandData.brandId}
+                    src={`/images/fb.svg`}
+                    className={`main-logo logo-name`}
+                    onClick={() => handleClick(brandData?.facebook, "FB")}
+                  />
+                  <img
+                    id={brandData.brandId}
+                    src={`/images/tele.svg`}
+                    className={`main-logo logo-name`}
+                    onClick={() => handleClick(brandData?.telegram, "TELE")}
+                  />
+                </div>
+              )}
+              {brandData.domain && (
+                <div className="footer">
+                  <div className="content">
+                    <div>
+                      <span className="highlight">{brandData.domain}</span> là
+                      tên miền mới nhất của{" "}
+                      <span className="highlight">{brandData.brandName}</span>
                     </div>
                   </div>
-                }
-                
-              </div>
+                  <div className="content">
+                    <div>
+                      Quý khách có thể truy cập{" "}
+                      <span className="highlight">
+                        <a href={`http://${brandData.brandName}`}>
+                          {brandData.brandName}
+                        </a>
+                      </span>{" "}
+                      để kiểm tra chính hãng.
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-          :
+        ) : (
           <div className="not-found-container">
             <div>
               <h1>404 - Page Not Found</h1>
-              <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+              <p>
+                The page you are looking for might have been removed, had its
+                name changed, or is temporarily unavailable.
+              </p>
             </div>
-            
           </div>
-        }
-        
+        )}
       </article>
     </div>
-    
   );
 }
 
