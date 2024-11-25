2bade42ccb2cb72daa2e0f3bced4749376a077d1
veadarasa <vea@darasa.io>
2024-11-25 16:41:10 +0400

    Change/[BEBO-5799][BC114103] Live chat page - update domain 102 - update env deploy
    change/[BEBO-5799][BC114103] Live chat page - update domain 102
    https://sgame.atlassian.net/browse/BEBO-5799

--- src/NavigateLiveChat.tsx
+++ src/NavigateLiveChat.tsx
@@ -78,8 +78,8 @@ function NavigateLiveChat() {
     },
     4102: {
       brandId: "4102",
-      brandName: "GO88.CA",
-      domain: "go88.ca",
+      brandName: "GO88.COM",
+      domain: "GO88.COM",
       phone: "0388.90.8888 0921.888.888",
       facebook: "https://www.facebook.com/go88chinhhang/",
       telegram: "https://t.me/Go88ChinhHang",
