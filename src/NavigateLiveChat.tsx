7e7dfa8de09c0eb652f697dd33aa23964b3fc020
veadarasa <vea@darasa.io>
2025-05-14 11:07:17 +0400

    - adđ new phone to brand top88
    - adđ new phone to brand top88

--- src/NavigateLiveChat.tsx
+++ src/NavigateLiveChat.tsx
@@ -115,7 +115,7 @@ function NavigateLiveChat() {
       brandId: "8116",
       brandName: "Top88",
       domain: "top88.vip",
-      phone: "",
+      phone: "0369.789.789",
       facebook: "http://fb.com/gamebaidaigiatop88",
       telegram: "https://t.me/top88gamebaidaigia",
     },
